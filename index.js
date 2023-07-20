7b47bec79f9a00e99d0a3d1efbbd7c613de7cbcd
Nikolai Guridov <125973969+guridovme@users.noreply.github.com>
2023-07-20 18:18:56 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+fetch ("https://dog.ceo/api/breeds/image/random")
+    .then(data => data.json())
+    .then(res => {
+        if (res.status !== 'success'){
+            return;
+        }
+        const imgSrc = res.message;
+
+        document.querySelector('.js-img').innerHTML = `
+        <img 
+        src = '${imgSrc}'
+        width = '500'
+        >
+        `;
+
+    });
